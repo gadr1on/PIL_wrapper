9bb9290be267f202ef1c00595c6a2e3b2b6c1823
gadr1on <gadrionpr@gmail.com>
2020-08-04 22:42:30 -0400

    .

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from distutils.core import setup
 setup(
   name = 'pilwrapper',         # How you named your package folder (MyLib)
   packages = ['pilwrapper'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Text wrapper for PIL/Pillow',   # Give a short description about your library
   author = 'gadrion',                   # Type in your name
   author_email = 'gradrionpr@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/gadr1on/pilwrapper',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/gadr1on/pilwrapper/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/gadr1on/pilwrapper/archive/v_03.tar.gz',
   keywords = ['pilwrapper', 'wrapper', 'pil', 'pillow', 'pillowwrapper'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'Pillow',
           'imageedit',
       ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+  classifiers=(
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Build Tools',
-    'License :: MIT License',
-    'Programming Language :: Python :: 3',
+    'Topic :: Software Development',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-  ],
+  ),
 )
